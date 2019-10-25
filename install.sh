826e87474d3d7c791e590381be8cb895e5941bbf
BENJAMIN LOPEZ <benjamin.lopez@worldline.com>
2019-10-25 15:57:57 +0200

    Add install scripts

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+if ! type curl > /dev/null; then
+  echo '[Error] This script requires curl. Exiting.'
+  exit 1
+fi
+
+if ! type zsh > /dev/null; then
+  echo '[Warning] zsh not installed.'
+fi
+
+
+if [[ -f .zshrc ]]; then
+  dest=".zshrc.$(date +%F).old"
+  echo "[Warning] .zshrc already exits. Moving it to $dest"
+  mv .zshrc $dest
+fi
+curl -o .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+
+for file in 'local' autosuggestions syntax-highlighting fzf-key-bindings fzf-key-completion; do
+  if [[ -f ".zshrc.$file" ]]; then
+    dest=".zshrc.$file.$(date +%F).old"
+    echo "[Warning] .zshrc.$file already exits. Moving it to $dest"
+    mv .zshrc $dest
+  fi
+
+  curl -o ".zshrc.$file" "https://raw.githubusercontent.com/scotow/zsh-setup/$file.zsh"
+done
