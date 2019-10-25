cb65b330f3d5b3b786acdfd376a4352a13f807db
BENJAMIN LOPEZ <benjamin.lopez@worldline.com>
2019-10-25 17:24:17 +0200

    Add curl redirection

--- install.sh
+++ install.sh
@@ -13,15 +13,15 @@ fi
 if [[ -f .zshrc ]]; then
   dest=".zshrc.$(date +%F).old"
   echo "[Warning] .zshrc already exits. Moving it to $dest"
-  mv .zshrc $dest
+  mv .zshrc $dest || exit 1
 fi
-curl -o .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+curl -sLo .zshrc https://git.grml.org/f/grml-etc-core/etc/zsh/zshrc || exit 1
 
 for file in 'local' autosuggestions syntax-highlighting fzf-key-bindings fzf-key-completion; do
   if [[ -f ".zshrc.$file" ]]; then
     dest=".zshrc.$file.$(date +%F).old"
     echo "[Warning] .zshrc.$file already exits. Moving it to $dest"
-    mv ".zshrc.$file" $dest
+    mv ".zshrc.$file" $dest || exit 1
   fi
-  curl -o ".zshrc.$file" "https://raw.githubusercontent.com/scotow/zsh-setup/master/$file.zsh"
+  curl -sLo ".zshrc.$file" "https://raw.githubusercontent.com/scotow/zsh-setup/master/$file.zsh" || exit 1
 done
